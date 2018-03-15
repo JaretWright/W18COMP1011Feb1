624e8a2a9dc48cb52db3ecbf8c42dcd82c08058c
JaretWright <hkkA223>
2018-03-15 16:52:07 -0400

    Added unique file names to images

--- src/utilities/FileMove.java
+++ src/utilities/FileMove.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.security.SecureRandom;
 
 /**
  *
@@ -22,14 +23,90 @@ public class FileMove
         //create a new Path to copy the image into a local directory
         Path sourcePath = imageFile.toPath();
         
-        //String uniqueFileName = getUniqueFileName(imageFile.getName());
+        String uniqueFileName = getUniqueFileName(imageFile.getName());
         
-        Path targetPath = Paths.get("./src/images/"+sourcePath.getFileName());
+        Path targetPath = Paths.get("./src/images/"+uniqueFileName);
         
         //copy the file to the new directory
         Files.copy(sourcePath, targetPath, StandardCopyOption.REPLACE_EXISTING);
         
         return new File(targetPath.toString());
+    }
+    
+
+    /**
+     * This method will receive a String that represents a file name and return a
+     * String with a random, unique set of letters prefixed to it
+     */
+    private static String getUniqueFileName(String oldFileName)
+    {
+        String newName;
+        
+        //create a Random Number Generator
+        SecureRandom rng = new SecureRandom();
+        
+        //loop until we have a unique file name
+        do
+        {
+            newName = "";
+            
+            //generate 32 random characters
+            for (int count=1; count <=32; count++)
+            {
+                int nextChar;
+                
+                do
+                {
+                    nextChar = rng.nextInt(123);
+                } while(!validCharacterValue(nextChar));
+                
+                newName = String.format("%s%c", newName, nextChar);
+            }
+            newName += ("_"+oldFileName);
+            
+        } while (!uniqueFileInDirectory(newName));
+        
+        return newName;
+    }
+    
+    /**
+     * This method will search the images directory and ensure that the file name
+     * is unique
+     */
+    public static boolean uniqueFileInDirectory(String fileName)
+    {
+        File directory = new File("./src/images/");
+        
+        File[] dir_contents = directory.listFiles();
+                
+        for (File file: dir_contents)
+        {
+            if (file.getName().equals(fileName))
+                return false;
+        }
+        return true;
+    }
+    
+    /**
+     * This method will validate if the integer given corresponds to a valid
+     * ASCII character that could be used in a file name
+     */
+    public static boolean validCharacterValue(int asciiValue)
+    {
+        
+        //0-9 = ASCII range 48 to 57
+        if (asciiValue >= 48 && asciiValue <= 57)
+            return true;
+        
+        //A-Z = ASCII range 65 to 90
+        if (asciiValue >= 65 && asciiValue <= 90)
+            return true;
+        
+        //a-z = ASCII range 97 to 122
+        if (asciiValue >= 97 && asciiValue <= 122)
+            return true;
+        
+        return false;
     }
     
     
